35684ab0834f06a809a1c7b16911b72347aebc60
Abbasali Nathani <abbasali.nathani@decos.com>
2026-01-30 01:28:18 +0530

    add logout, nav bar

--- src/app/components/main-menu/main-menu.routes.ts
+++ src/app/components/main-menu/main-menu.routes.ts
@@ -2,6 +2,13 @@ import { Routes } from "@angular/router";
 import { PathNames } from "../../shared/constants";
 
 export const mainMenuRoutes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: PathNames.DASHBOARD },
-    { path: PathNames.DASHBOARD, pathMatch: 'full', loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard) },
+    {
+        path: '',
+        loadComponent: () => import('./main-menu').then(m => m.MainMenu),
+        children: [
+            { path: '', redirectTo: PathNames.DASHBOARD, pathMatch: 'full' },
+            { path: PathNames.DASHBOARD, loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard) },
+            { path: PathNames.FILES, loadComponent: () => import('./files/files').then(m => m.Files) },
+        ]
+    },
 ]
--- src/app/components/main-menu/main-menu.ts
+++ src/app/components/main-menu/main-menu.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideLogOut } from '@ng-icons/lucide';
+import { PathNames } from '../../shared/constants';
 
 @Component({
   selector: 'app-main-menu',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmIcon, NgIcon, RouterLinkWithHref],
+  providers: [provideIcons({ lucideLogOut })],
   templateUrl: './main-menu.html',
   styleUrl: './main-menu.scss',
 })
-export class MainMenu {
+export class MainMenu implements OnInit {
 
+  public authService = inject(AuthService);
+
+  private cdr = inject(ChangeDetectorRef);
+  private router = inject(Router);
+
+  public PathNames = PathNames;
+
+  ngOnInit(): void {
+    this.checkAndFetchUserDetails();
+  }
+
+  private checkAndFetchUserDetails(): void {
+    if (this.authService.isAuthenticated) {
+      if (this.authService.currentUser === null) {
+        this.authService.getLoggedInUserDetails().subscribe({
+          next: (userDetails) => {
+            this.authService.currentUser = userDetails;
+            this.cdr.detectChanges();
+          },
+          error: () => {
+            console.error('Failed to fetch user details');
+          }
+        });
+      } else {
+        this.authService.currentUser = this.authService.currentUser;
+        this.cdr.detectChanges();
+      }
+    }
+  }
+
+  public logout(): void {
+    this.authService.logout();
+  }
+
+  public redirectToDashboard(): void {
+    this.router.navigate([PathNames.MAIN, PathNames.DASHBOARD]);
+  }
 }
